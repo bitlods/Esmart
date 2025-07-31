8fdec757fef0a9fb5401192e8238ffdd611117b8
bitlods <ptharun622@gmail.com>
2025-07-31 11:37:00 +0530

    Milestones 4 : Complete my Front end with the thymeLeaf Css and also connect with my controller

--- Smart/src/main/java/com/Tharun/Product/Controller/ProductController.java
+++ Smart/src/main/java/com/Tharun/Product/Controller/ProductController.java
@@ -1,15 +1,20 @@
 package com.Tharun.Product.Controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.Tharun.Product.Entity.product;
 import com.Tharun.Product.Service.ProductService;
+
 
 @RestController
 @RequestMapping("/product")
@@ -21,8 +26,21 @@ public class ProductController {
 	@GetMapping("/form")
 	public String productForm(Model model) {
 		product p=new product();
-		model.addAttribute("employee", p);
+		model.addAttribute("product", p);
 		return "Product-form";	
 	}
 
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute product p,RedirectAttributes attr) {
+		sev.saveProduct(p);
+		attr.addFlashAttribute("success","Saved Successfully");
+		return "redirect:/product/plist";	
+	}
+	
+	@GetMapping("/plist")
+    public String  getEmployeeList(Model model) {
+    	List<product> plist=sev.getAllEmployee();
+    	model.addAttribute("allproduct",plist);
+    	return "plist";
+    }
 }
