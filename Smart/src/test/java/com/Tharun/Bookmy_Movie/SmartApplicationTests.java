506832b59822c19b51dc71503ae4b45114148fca
bitlods <ptharun622@gmail.com>
2025-07-31 10:56:03 +0530

    Milestone 1: Databeses created and push to my github

---
+++ Smart/src/test/java/com/Tharun/Bookmy_Movie/SmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tharun.Bookmy_Movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
