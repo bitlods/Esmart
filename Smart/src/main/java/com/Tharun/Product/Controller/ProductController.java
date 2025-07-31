eb7e51e2750d08e458e3d994cc268f883d7a8280
bitlods <ptharun622@gmail.com>
2025-07-31 11:23:50 +0530

    Milestones 3 : Complete my Api Contoller

--- Smart/src/main/java/com/Tharun/Product/Controller/ProductController.java
+++ Smart/src/main/java/com/Tharun/Product/Controller/ProductController.java
@@ -1,5 +1,28 @@
 package com.Tharun.Product.Controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Tharun.Product.Entity.product;
+import com.Tharun.Product.Service.ProductService;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+	
+	@Autowired
+	ProductService sev;
+	
+	@GetMapping("/form")
+	public String productForm(Model model) {
+		product p=new product();
+		model.addAttribute("employee", p);
+		return "Product-form";	
+	}
 
 }
